2eb845b60ecc72b5a93d2102e9c222e1a8b7067f
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2024-07-25 00:24:28 +0430

    Add project files.

---
+++ BridgePattern/Bridge/Implementors/Implementor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern.Bridge.Implementors
+{
+    public abstract class Implementor
+    {
+        public abstract void Implementation();
+    }
+    public class ConcreateImolementor : Implementor
+    {
+        public override void Implementation()
+        {
+            Console.WriteLine("ConcreateImolementorClass");
+        }
+    }
+}
