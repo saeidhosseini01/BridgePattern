169dc3f9d141ae4b8a4819e8c0d753422757ca07
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2024-07-25 00:34:10 +0430

    syncAbstractClass

---
+++ BridgePattern/Bridge/Abstracctions/Abstraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern.Bridge.Abstracctions
+{
+    public abstract class Abstraction
+    {
+        public void Function() { }
+    }
+
+    public class ReFindAbstraction: Abstraction
+    {
+
+    }
+}
